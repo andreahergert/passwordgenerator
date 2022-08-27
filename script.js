3e5fba7896d7cdeaed38fc421d7cba9bfccd3a8a
Andrea Hergert <ahergert24@hotmail.com>
2022-08-27 12:14:39 -0700

    added fuction to generatePassword and first promt

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+function generatePassword() {
+
+  var userInput = window.prompt("How long do you want you password to be?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("This is not a number!")
+    return
+  }
+
+  
+
+
+
+}
+
+// Writes password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
